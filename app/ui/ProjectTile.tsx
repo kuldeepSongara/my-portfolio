30a3e6d86f411d27f9fe884718de7f82b1f28d41
Kuldeep Songara <kuldeepsongaraz@gmail.com>
2023-11-04 20:20:12 +0530

    tile color change

--- app/ui/ProjectTile.tsx
+++ app/ui/ProjectTile.tsx
@@ -3,7 +3,7 @@ import { Project } from "@/app/lib/definitions";
 const ProjectTile = ({ projectDetail }: { projectDetail: Project }) => {
   const { tech, name, description, techStack } = projectDetail;
   return (
-    <section className="bg-[#030303] p-6">
+    <section className="bg-[#090909] p-6">
       <h6 className="uppercase text-xs tracking-widest">{tech}</h6>
       <p className="mt-5 text-3xl tracking-normal text-slate-300">{name}</p>
       <p className="text-sm tracking-wider mt-3 text-slate-400">
